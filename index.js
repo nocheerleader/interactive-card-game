9db571bcaf46c30e5427c13093696b04db580c04
Ubuntu <ubuntu@ip-172-31-38-207.ec2.internal>
2019-10-06 09:24:46 +0000

    set up index.js async loading

---
+++ index.js
@@ -0,0 +1,23 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('start-prompt'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+
+        });
+    });
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+
+        });
+    });
+}
