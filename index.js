44788d00ed3539fb95a3e267a57b0b93ad13b912
Ubuntu <ubuntu@ip-172-31-38-207.ec2.internal>
2019-10-06 10:10:38 +0000

    add audio files and functions

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+class AudioController {
+    constructor() {
+        this.music = new Audio('assets/audio/cartoon_music.mp3');
+        this.flipAudio = new Audio('assets/audio/card_flip.wav');
+        this.matchAudio = new Audio('assets/audio/card_match.mp3');
+        this.winnerAudio = new Audio('assets/audio/winner.wav');
+        this.gameOverAudio = new Audio('assets/audio/game_over.wav');
+        this.music.volume = 0.3;
+        this.music.loop = true;
+    }
+    startMusic() {
+        this.music.play();
+    }
+    stopMusic() {
+        this.music.pause();
+        this.music.currentTime = 0;
+    }
+    flip() {
+        this.flipAudio.play();
+    }
+    match() {
+        this.matchAudio.play();
+    }
+    victory() {
+        this.stopMusic();
+        this.winnerAudio.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverAudio.play();
+    }
+}
+
+
+
+
 if (document.readyState == 'loading') {
     document.addEventListener('DOMContentLoaded', ready);
 } else {
@@ -21,3 +57,4 @@ function ready() {
         });
     });
 }
+
